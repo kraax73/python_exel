041c9150f59a3e6fe539d57871fdc578c94573d5
kraax73 <kickslove7300@gmail.com>
2021-07-25 14:24:01 +0900

    fix

--- script.py
+++ script.py
@@ -1,12 +1,13 @@
 # coding: UTF-8
 
 import openpyxl
-
+#デスクトップ上の既存Exelファイルの取得
 wb = openpyxl.load_workbook('/Users/kicks-t73/Desktop/test.xlsx')
-
+#ファイル内のシートを名前で取得＆名前変更
 ws = wb["Sheet1"]
 ws.title = "new"
 
+#保存
 wb.save('/Users/kicks-t73/Desktop/test.xlsx')
 wb.close()
 
