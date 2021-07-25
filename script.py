a499c4305b865dbaf3ece51afc0ed6a8fcd6eaca
kraax73 <kickslove7300@gmail.com>
2021-07-25 14:21:49 +0900

    fix

--- script.py
+++ script.py
@@ -2,4 +2,11 @@
 
 import openpyxl
 
+wb = openpyxl.load_workbook('/Users/kicks-t73/Desktop/test.xlsx')
+
+ws = wb["Sheet1"]
+ws.title = "new"
+
+wb.save('/Users/kicks-t73/Desktop/test.xlsx')
+wb.close()
 
